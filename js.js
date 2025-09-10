81ff4b9ecbd042341d9edb63c56b066ed1136cd9
martinasemper <49085852@ivt.edu.ar>
2025-09-09 21:04:25 -0300

    por fin terminado

--- js.js
+++ js.js
@@ -1,40 +1,40 @@
+ let key = "ODpC2hzlukHNuJrBnBF022adtR8fP1F9jX81e0qO";
 
 
-let imagenDeldia= document.querySelector('#imagenDeldia')
-let boton = document.querySelector('#boton')
-let titulo =document.querySelector('h1')
-boton.onclick=function () {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=ODpC2hzlukHNuJrBnBF022adtR8fP1F9jX81e0qO')
-    .then(res=>res.json())
-    .then(fotoDeldia=>{
-imagenDeldia.src = fotoDeldia.hdurl
-titulo.textContent = fotoDeldia.title
-    }
+    document.querySelector('#btnDia').onclick = () => {
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+        .then(res => res.json())
+        .then(data => {
+          document.querySelector('#imgDia').src = data.url;
+          document.querySelector('#tituloDia').textContent = data.title;
+        });
+    };
 
-    )
-}
-let imagenFecha=document.querySelector('#imagenFecha')
-let botonFecha= document.querySelector('#botonFecha')
-let fechaUsuario= document.querySelector('#fecha')
-boton.onclick=function () {
-    console.log(fechaUsuario)
-  fetch ('https://api.nasa.gov/planetary/apod?api_key=${key}&date={fechaUsuario.value}')
-  .then(res=>json())
-  .then(fotoFecha.src = fotoFecha.hdurl)
-}
- let contenedorImagen = document.querySelector('.contenedorImagen')
- let botonCantidad= document.querySelector('#botonCantidad')
- let cantidadUsuario=document.querySelector('#cantidad')
- botonCantidad.onclick=function () {
-    console.log(cantidadUsuario.value)
-    fetch('https://api.nasa.gov/planetary/apod?api_key=${key}&count=${catidadUsuario}')
-    .then(res=json())
-    .then(fotos=>
-        contenedorImagen.innerHTML= ""
-        for (let i = 0; i < fotos.length; i++) {
-       contenedorImagen.innerHTML=${contenedorImagen.innerHTML} <img src="${fotos[i].url}"alt="${fotos[i].title}"
-            
-        }
-        
-    )
- }
+
+    document.querySelector('#btnFecha').onclick = () => {
+      const fecha = document.querySelector('#fecha').value;
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${fecha}`)
+        .then(res => res.json())
+        .then(data => {
+          document.querySelector('#imgFecha').src = data.url;
+          document.querySelector('#tituloFecha').textContent = data.title;
+        });
+    };
+
+  
+    document.querySelector('#btnCantidad').onclick = () => {
+      const cantidad = document.querySelector('#cantidad').value;
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&count=${cantidad}`)
+        .then(res => res.json())
+        .then(data => {
+          const contenedor = document.querySelector('#contenedor');
+          contenedor.innerHTML = "";
+          data.forEach(foto => {
+            contenedor.innerHTML += `<img src="${foto.url}" alt="${foto.title}" width="250"> `;
+          });
+        });
+    };
+
+
+
+
